e8a1cf41b49b10a40a2effb2eeefd4a2379e1f3e
shaun <shaunosullivan90@live.com>
2023-04-07 23:41:57 +1000

    Further updates to core structure

--- ChilliTracker.Shared/Connection/IMongoDatabaseConnection.cs
+++ ChilliTracker.Shared/Connection/IMongoDatabaseConnection.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ChillTracker.API.Connection
+namespace ChillTracker.Shared.Connection
 {
     public interface IMongoDatabaseConnection
     {
