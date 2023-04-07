67f6bfe3c551c097ab5756b41448c476079fda56
Shaun O'Sullivan <shaunosullivan90@live.com>
2023-04-07 21:17:48 +1000

    Repository Setup - no methods

---
+++ ChilliTracker.Business/Interfaces/IChilliPlantRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChilliTracker.Business.Interfaces
+{
+    interface IChilliPlantRepository
+    {
+        // Get All Chillies
+        // Get All Chillies with Filter
+        // All Chillies with Issues
+        // All Chillies Potted up more than 6 months ago (due for upgrade)
+        // All Chillies Not Germinated
+        // + More
+        // Add new Chilli
+        // Add new Harvest Event to Chilli 
+        // Add new Potting Event to Chilli
+        // Add new Issue to Chilli
+        // Update Chilli details
+        // Delete Chilli
+        // Remove Chilli Harvest Event
+        // Remove Chilli Potting Event
+        // Remove Chilli Issue
+    }
+}
---
+++ ChilliTracker.Business/Repository/ChilliPlantRepository.cs
@@ -0,0 +1,13 @@
+﻿using ChilliTracker.Business.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChilliTracker.Business.Repository
+{
+    public class ChilliPlantRepository : IChilliPlantRepository
+    {
+    }
+}
